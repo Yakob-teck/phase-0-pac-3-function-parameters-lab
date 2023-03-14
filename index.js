8b849277f18e97bc00f547a44ee44e23b94f48f9
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-14 17:35:36 -0600

    parameter lab

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function introduction(name){
-    return `Hi, my name is ${name}.`;
+    const string= `Hi, my name is ${name}.`;
+    return string;
 
 }
 function introductionWithLanguage(name,language){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-
+    const phrase=`Hi, my name is ${name} and I am learning to program in ${language}.`;
+return phrase;
 }
-function introductionWithLanguageOptional(name,language ='Javascript'){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-
+function introductionWithLanguageOptional(name, language="JavaScript"){
+   const result= `Hi, my name is ${name} and I am learning to program in ${language}.`;
+return result;
 }
